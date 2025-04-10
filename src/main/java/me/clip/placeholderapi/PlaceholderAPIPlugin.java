5497c230a8c5a16f11f6a3cc2d0871a8cba2f67c
Blacky <64606171+Blaackyy@users.noreply.github.com>
2025-04-10 11:11:19 -0400

    Missing scheduler change

--- src/main/java/me/clip/placeholderapi/PlaceholderAPIPlugin.java
+++ src/main/java/me/clip/placeholderapi/PlaceholderAPIPlugin.java
@@ -271,8 +271,7 @@ public final class PlaceholderAPIPlugin extends JavaPlugin {
       Class.forName("org.bukkit.event.server.ServerLoadEvent");
       new ServerLoadEventListener(this);
     } catch (final ClassNotFoundException ignored) {
-      Bukkit.getScheduler()
-          .runTaskLater(this, () -> getLocalExpansionManager().load(Bukkit.getConsoleSender()), 1);
+      getFoliaLib().getScheduler().runNextTick((task) -> getLocalExpansionManager().load(Bukkit.getConsoleSender()));
     }
   }
 
